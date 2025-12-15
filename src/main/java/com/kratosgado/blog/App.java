28869a5c5ed6fb3002d07410cfdb2a7d99119da1
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-15 16:09:54 +0000

    add (main): materialfx dependency

--- src/main/java/com/kratosgado/blog/App.java
+++ src/main/java/com/kratosgado/blog/App.java
@@ -1,5 +1,6 @@
 package com.kratosgado.blog;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -16,7 +17,9 @@ public class App extends Application {
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
     Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+    MFXButton button = new MFXButton("Click me!");
+    button.setOnAction(e -> l.setText("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "."));
+    Scene scene = new Scene(new StackPane(button), 640, 480);
     stage.setScene(scene);
     stage.show();
   }
