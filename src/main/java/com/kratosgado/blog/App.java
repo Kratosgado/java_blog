32ab42e2464ec0facd554c63733dd6ea0c9885b6
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-16 17:18:25 +0000

    setup(main):lombok dependency

--- src/main/java/com/kratosgado/blog/App.java
+++ src/main/java/com/kratosgado/blog/App.java
@@ -1,10 +1,13 @@
 package com.kratosgado.blog;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
+// import io.github.palexdev.materialfx.theming.Themes;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,15 +16,18 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
   @Override
-  public void start(Stage stage) {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    MFXButton button = new MFXButton("Click me!");
-    button.setOnAction(e -> l.setText("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "."));
-    Scene scene = new Scene(new StackPane(button), 640, 480);
-    stage.setScene(scene);
-    stage.show();
+  public void start(Stage primaryStage) throws Exception {
+
+    UserAgentBuilder.builder().themes(JavaFXThemes.MODENA)
+        .themes(MaterialFXStylesheets.forAssemble(true))
+        .setDeploy(true)
+        .setResolveAssets(true).build().setGlobal();
+
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+    Scene scene = new Scene(root, 600, 500);
+    primaryStage.setTitle("Blog");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
