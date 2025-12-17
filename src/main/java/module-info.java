31769c4b06286b1f293e3e06f2349524511161dc
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-17 09:22:28 +0000

    fix(feat/auth): fxml bugs

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.kratosgado.blog {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires MaterialFX;
+  requires java.sql;
+  requires static lombok;
+  // requires lombok;
+
+  opens com.kratosgado.blog.controllers to javafx.fxml;
+  opens com.kratosgado.blog.dao to java.sql;
+  // opens com.kratosgado.blog to lombok;
+
+  exports com.kratosgado.blog;
+}
