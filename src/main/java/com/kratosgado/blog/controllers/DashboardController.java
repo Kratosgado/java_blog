a61186b790e173560989738e3454b8830d8bb9bc
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-18 14:44:55 +0000

    impl(feat/dashboard): connect dashboard-home to dashboard

--- src/main/java/com/kratosgado/blog/controllers/DashboardController.java
+++ src/main/java/com/kratosgado/blog/controllers/DashboardController.java
@@ -1,6 +1,19 @@
 
 package com.kratosgado.blog.controllers;
 
+import com.kratosgado.blog.utils.Navigator;
+import com.kratosgado.blog.utils.Routes;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+
 public class DashboardController {
 
+  @FXML
+  private StackPane contentArea;
+
+  @FXML
+  private void initialize() {
+    contentArea.getChildren().add(Navigator.getInstance().getSubScene(Routes.DASHBOARD_HOME));
+  }
 }
