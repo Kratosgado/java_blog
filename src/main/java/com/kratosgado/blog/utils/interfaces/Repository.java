a3eee98356e827f363bf21d391eef5168e48436d
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-18 16:55:11 +0000

    impl(feat/dashboard): add UIS

---
+++ src/main/java/com/kratosgado/blog/utils/interfaces/Repository.java
@@ -0,0 +1,54 @@
+
+package com.kratosgado.blog.utils.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T> {
+
+  /**
+   * Adds the given entity to the database
+   * 
+   * @param model
+   * @return the entity added to the database
+   */
+  public T add(T model);
+
+  public T update(T model);
+
+  /**
+   * @return List of all entities in the database
+   */
+  public List<T> getAll();
+
+  /**
+   * Returns the entity with the given id
+   * 
+   * @param id
+   * @return Optional containing the entity with the given id
+   * @throws RuntimeException
+   */
+  public Optional<T> getById(String id) throws RuntimeException;
+
+  /**
+   * Removes the entity with the given id from the database
+   * 
+   * @param id
+   * @return true if the entity was removed, false otherwise
+   */
+  public boolean removeById(String id);
+
+  /**
+   * Checks if the entity with the given id exists in the database
+   * 
+   * @param id
+   * @return true if the entity exists, false otherwise
+   */
+  public boolean exists(String id);
+
+  /**
+   * @return number of entities in the database
+   */
+  public int count();
+
+}
