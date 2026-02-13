29dcf6866c2a1b987c6bdc92b5b4a5188390025b
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-13 08:49:37 +0000

    refactor(oauth): remove explicit definition of controller constructors

--- blog-backend/src/main/java/com/kratosgado/blog/backend/graphql/AuthGraphQLController.java
+++ blog-backend/src/main/java/com/kratosgado/blog/backend/graphql/AuthGraphQLController.java
@@ -1,38 +1,27 @@
 package com.kratosgado.blog.backend.graphql;
 
+import com.kratosgado.blog.backend.services.AuthService;
+import com.kratosgado.blog.dtos.request.LoginRequest;
+import com.kratosgado.blog.dtos.request.RegisterRequest;
+import com.kratosgado.blog.dtos.response.AuthResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
-import com.kratosgado.blog.backend.security.JwtUtil;
-import com.kratosgado.blog.backend.services.AuthService;
-import com.kratosgado.blog.dtos.request.LoginRequest;
-import com.kratosgado.blog.dtos.request.RegisterRequest;
-import com.kratosgado.blog.dtos.response.AuthPayload;
-import com.kratosgado.blog.models.User;
-
 @Controller
+@RequiredArgsConstructor
 public class AuthGraphQLController {
 
   private final AuthService authService;
-  private final JwtUtil jwtUtil;
 
-  public AuthGraphQLController(AuthService authService, JwtUtil jwtUtil) {
-    this.authService = authService;
-    this.jwtUtil = jwtUtil;
+  @MutationMapping
+  public AuthResponse register(@Argument RegisterRequest input) {
+    return authService.register(input);
   }
 
   @MutationMapping
-  public AuthPayload register(@Argument RegisterRequest input) {
-    User user = authService.register(input);
-    String token = jwtUtil.generateToken(user.getEmail(), Long.valueOf(user.getId()));
-    return new AuthPayload(token, user);
-  }
-
-  @MutationMapping
-  public AuthPayload login(@Argument LoginRequest input) {
-    User user = authService.login(input);
-    String token = jwtUtil.generateToken(user.getEmail(), Long.valueOf(user.getId()));
-    return new AuthPayload(token, user);
+  public AuthResponse login(@Argument LoginRequest input) {
+    return authService.login(input);
   }
 }
--- blog-backend/src/main/java/com/kratosgado/blog/backend/graphql/CommentGraphQLController.java
+++ blog-backend/src/main/java/com/kratosgado/blog/backend/graphql/CommentGraphQLController.java
@@ -1,8 +1,16 @@
 package com.kratosgado.blog.backend.graphql;
 
+import com.kratosgado.blog.backend.services.CommentService;
+import com.kratosgado.blog.backend.services.PostService;
+import com.kratosgado.blog.dtos.request.CreateCommentRequest;
+import com.kratosgado.blog.dtos.request.PageRequest;
+import com.kratosgado.blog.dtos.response.PageResponse;
+import com.kratosgado.blog.dtos.response.PostResponse.PostDetails;
+import com.kratosgado.blog.models.Comment;
+import com.kratosgado.blog.models.User;
 import java.util.Collections;
 import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -10,26 +18,12 @@ import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
-import com.kratosgado.blog.backend.services.CommentService;
-import com.kratosgado.blog.backend.services.PostService;
-import com.kratosgado.blog.dtos.request.CreateCommentRequest;
-import com.kratosgado.blog.dtos.request.PageRequest;
-import com.kratosgado.blog.dtos.response.PageResponse;
-import com.kratosgado.blog.dtos.response.PostResponse.PostDetails;
-import com.kratosgado.blog.dtos.response.PostResponse.PostView;
-import com.kratosgado.blog.models.Comment;
-import com.kratosgado.blog.models.User;
-
 @Controller
+@RequiredArgsConstructor
 public class CommentGraphQLController {
 
   private final CommentService commentService;
   private final PostService postService;
-
-  public CommentGraphQLController(CommentService commentService, PostService postService) {
-    this.commentService = commentService;
-    this.postService = postService;
-  }
 
   @QueryMapping
   public Comment comment(@Argument String id) {
@@ -38,14 +32,13 @@ public class CommentGraphQLController {
 
   @QueryMapping
   public PageResponse<Comment> commentsByPost(
-      @Argument Long postId,
-      @Argument(name = "page") int page,
-      @Argument(name = "size") int size) {
+      @Argument Long postId, @Argument(name = "page") int page, @Argument(name = "size") int size) {
     return commentService.getPostComments(postId, new PageRequest(page, size, "id", "desc"));
   }
 
   @MutationMapping
-  public Comment createComment(@Argument CreateCommentRequest input, @AuthenticationPrincipal User user) {
+  public Comment createComment(
+      @Argument CreateCommentRequest input, @AuthenticationPrincipal User user) {
 
     return commentService.createComment(input, user);
   }
